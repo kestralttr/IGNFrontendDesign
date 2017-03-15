ade1e40cee03932d86d6a18fdf3798967e3db2ad
Alex Bennett <kestralttr@gmail.com>
2017-03-14 21:59:26 -0700

    initial commit

---
+++ javascript.js
@@ -0,0 +1,14 @@
+
+// $.ajax({
+//   type: "GET",
+//   url: "https://ign-apis.herokuapp.com/articles",
+//   crossDomain: true,
+//   dataType: 'jsonp',
+//   success: function(result) {
+//     let headline = result.data[0].metadata.headline;
+//     $(document.body).append(headline);
+//   },
+//   error: function() {
+//     console.log("didn't work");
+//   }
+// });
