d8dcafae0a1fb66a0627d8b382213a7dad55c9bb
Alex Bennett <kestralttr@gmail.com>
2017-03-14 23:30:39 -0700

    establishes rough style, and begins adding jQuery code

--- javascript.js
+++ javascript.js
@@ -12,3 +12,32 @@
 //     console.log("didn't work");
 //   }
 // });
+
+//list-item (<li>)
+  //list-item-number (<div>)
+    //(<p>)
+  //list-item-text (<div>)
+    //(<p>)
+  //list-item-time (<div>)
+    //(<p>)
+
+  let listItem = "<li class='list-item'</li>";
+  $("#list").append(listItem);
+
+  let listItemNumber = "<div class='list-item-number'></div>";
+  $(".list-item:last").append(listItemNumber);
+
+  let listItemText = "<div class='list-item-text'></div>'";
+  $(".list-item:last").append(listItemText);
+
+  let listItemTime = "<div class='list-item-time'></div>'";
+  $(".list-item:last").append(listItemTime);
+
+  let listItemNumberPTag = "<p>02</p>";
+  $(".list-item-number:last").append(listItemNumberPTag);
+
+  let listItemTextPTag = "<p>This text is from jQuery!<br>And so is this!</p>";
+  $(".list-item-text:last").append(listItemTextPTag);
+
+  let listItemTimePTag = "<p>2:39</p>";
+  $(".list-item-time:last").append(listItemTimePTag);
